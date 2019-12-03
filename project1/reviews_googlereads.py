074986432016572767ed9c012dffd9db14a7368d
TailaneBrito <tailane.brito01@gmail.com>
2019-12-03 12:19:42 -0500

    progressing with googlereads api

--- project1/reviews_googlereads.py
+++ project1/reviews_googlereads.py
@@ -1,14 +1,22 @@
 import requests
 
-res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "ILwXvguynynBDzXQmVmUQ",
-                                                                                "isbns": "1632168146",
-                                                                                },
-                   verify=False
-                   )
-print(res.json())
-print(res.json().books)
+def main():
+    key = 'ILwXvguynynBDzXQmVmUQ'
+    isbn = '1632168146'
+    res = requests.get("https://www.goodreads.com/book/review_counts.json",
+                       params={"key": key,
+                               "isbns": isbn},
+                       verify=False
+                       )
+    if res.status_code != 200:
+        raise Exception("ERROR: API request unsuccessful.")
 
-print(res.cookies)
+    data = res.json()
+    book_rating = data["books"][9]
+    print(f"1 {isbn} has the {book_rating} on Googlereads!")
+
+if __name__ == "__main__":
+    main()
 
 
 #verify='/usr/local/Cellar/python/3.7.5/Frameworks/Python.framework/Versions/3.7/lib/python3.7/site-packages/requests/cacert.pem'
