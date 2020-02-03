f15dd6a83a9b6a77a7e4ddae1a1a0563d8acc31a
TailaneBrito <tailane.brito01@gmail.com>
2020-02-03 15:13:24 -0500

    okay, vamos tentar

--- Project2v1/application.py
+++ Project2v1/application.py
@@ -3,9 +3,7 @@ import requests
 import json
 
 import functools
-# format the way time is displayed
 from time import localtime, strftime
-
 from flask_login import LoginManager, current_user, login_required, user_logged_out, user_logged_in
 from flask import Flask, render_template, session, url_for, request, flash, redirect
 from flask_session import Session
@@ -47,7 +45,7 @@ def load_user(user_name):
 @app.route("/")
 def index():
 
-    return render_template("dashboard.html", rooms=ROOMS)
+    return render_template("dashboard.html", rooms=ROOMS, room=session["user_room"])
 
 ''' 
 @app.route("/logado")
@@ -58,22 +56,18 @@ def index():
 
 @app.route('/chat-room',  methods=['POST', 'GET'])
 def chat():
-    print('chat() function ')
+
     room = request.form.get('room_name')
+    print(room)
     name = session['user_name']
+    print(name)
 
     json = {
         "user_name": name,
         "room": room
     }
 
-    return render_template("index.html", json=json, user=name, users=users, room=room, rooms=ROOMS)
-
-
-@app.route('/chat-room')
-def chat_room():
-    print('veio aqui')
-
+    return render_template("index.html", json=json, user=name, users=users, room=room)
 
 @app.route('/chat-room')
 def leaveTheRoom(json):
@@ -91,54 +85,17 @@ def new_user(name):
     emit('user-connected', name, broadcast=True)
 
 
-@socketio.on('connect', socketio)
-def connect(socketio):
-
-    endp = socketio.manager.handshaken[socketio.id].address
-    print(f"{endp} caminho")
-    #users.push(socketio)
-
-    mensagem = request.form.get('message-input')
-    room = request.form.get('room')
-
-    msg = {"message": mensagem}
-    socketio.to(room).emit('test-chat-message', msg, broadcast=True)
-
-
-@socketio.on('message')
-def send_message(data):
-    print('getting the message from send_message ' + str(data))
-    mensagem = request.form.get('message-input')
-    room = request.form.get('room')
-
-    send({"message": data["message"],
-          "username": data["user_name"],
-          "time_stamp": strftime('%b-%d %I:%M%p', localtime())
-          })
-
-    msg = {"message": mensagem}
-
-
-    socketio.emit('test-chat-message', msg, broadcast=True)
-
-
 @socketio.on('send-chat-message')
 def send_chat_message(json, methods=['GET', 'POST']):
     #socketio.emit('chat-message', json, callback=messageReceived)
-    print(str(json))
 
     json = {'message': json['message'],
             'user_name': json['user_name'],
             'timestamp': strftime('%b-%d %I:%M%p', localtime()),
             'room': session['user_room']}
 
-
-    print('received by send-chat-message server-side' + str(json))
-
-
+    print('received message by send-chat-message ' + str(json))
     socketio.emit('chat-message', json, callback=messageReceived)
-    #socketio.emit('chat-message', json, namespace=json['room'], callback=messageReceived)
-    #socketio.emit('chat-message', json, callback=messageReceived, namespace=json['room'])
 
 
 @socketio.on('my event')
@@ -153,22 +110,17 @@ def handle_messages_custom_event(json, methods=['GET', 'POST']):
 # Session
 @socketio.on('get-user')
 def user_section(json, methods=['GET', 'POST']):
-    ''' it is called in dashboard.js '''
+    ''' action on dashboard.js connect '''
     print('get-user application ' + str(json))
     name = json['user_name']
     room = json['room']
     sid = request.sid
-    users[name] = {"user_name": name,
-                   "room": room,
-                   "user_sid": sid
-                   }
+    users[name] = room
 
-    #flask login session
     login_user = User(name, room, sid)
 
     if not name in session:
         # return render_template("error.html", message="this user is already in session")
-        print("user not in session adding it now")
         session['user_name'] = name
         session['user_room'] = room
         session['user_sid'] = sid
@@ -179,43 +131,28 @@ def user_section(json, methods=['GET', 'POST']):
                 }
         print(str(json))
     else:
-        # If user message isn't attached to a user name.
-        print("else")
         return url_for("chat")
 
     validate_user_section()
     on_join(json)
-
     socketio.emit('get-user-info', json, room=room)
 
 
 
 @socketio.on('join')
 def on_join(data):
-    print(str(data))
-
-    print(f"{data['user_name']} entrou na sala {data['room']} on_join()")
-
-    #join the room. It is received the message
-    join_room(data['room'], sid=data['sid'], namespace=data['room'])
-
-    #socketio.send('chat-message', {'message': data['user_name'] + " has joined the " + data['room'] + " room "},
-    #              room=data['room'], namespace=data['room'])
-
-    send({'message': data['user_name'] + " has joined the " + data['room'] + " room "}, room=data['room'])
+    print(data['room'])
+    join_room(data['room'])
+    socketio.emit('chat-message', {'message': data['user_name'] + " has joined the " + data['room'] + " room "})
 
 @socketio.on('leave')
 def on_leave(data):
     ''' Users leave the room '''
     username = str(data['user_name'])
     room = str(data["room"])
+
     print(username + " is leaving the " + room + " room ")
-
-    leave_room(room=data['room'], namespace=data['room'])
-
-    #socketio.emit('chat-message', {'message': username + " has left the " + room + " room"})
-    send('chat-message', {'message': username + " has left the " + room + " room"})
-
+    socketio.emit('chat-message', {'message': username + " has left the " + room + " room"})
 
 socketio.on('redirect-dashboard')
 def on_redirect_dash():
